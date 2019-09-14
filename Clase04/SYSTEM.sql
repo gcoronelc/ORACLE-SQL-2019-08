de1bfe5153174d9ebff0f9757b233ebb7c32b16b
Gustavo Coronel <gcoronelc@gmail.com>
2019-09-14 19:54:46 +0200

    Clase 04

---
+++ Clase04/SYSTEM.sql
@@ -0,0 +1,7 @@
+select * from scott.emp;
+
+select * from eureka.cuenta where chr_cuencodigo='00100002';
+select * from eureka.movimiento where chr_cuencodigo='00100002';
+
+select * from scott.emp for update;
+
